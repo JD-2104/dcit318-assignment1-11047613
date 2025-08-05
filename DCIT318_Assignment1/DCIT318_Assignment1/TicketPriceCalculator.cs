73cff60a6b7d84618f065b894f4cf532f8cf9be9
JD-2104 <jdanso019@st.ug.edu.gh>
2025-08-05 02:53:32 +0000

    I added Ticket Price calculator

---
+++ DCIT318_Assignment1/DCIT318_Assignment1/TicketPriceCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class TicketPriceCalculator
+{
+    public static void Run()
+    {
+        const double regularPrice = 10.0;
+        const double discountPrice = 7.0;
+
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        double price;
+
+        if (age <= 12 || age >= 65)
+            price = discountPrice;
+        else
+            price = regularPrice;
+
+        Console.WriteLine($"Your ticket price is: GHC{price}");
+    }
+}
